da9ec20dfd2af7eac73199c27d40e8609e46270c
ml5191 <ml5191@columbia.edu>
2024-11-14 23:20:08 -0500

    new style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
+// @ts-ignore
 import { defineConfig } from "astro/config";
+// @ts-ignore
 import mdx from "@astrojs/mdx";
+// @ts-ignore
 import sitemap from "@astrojs/sitemap";
+// @ts-ignore
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
